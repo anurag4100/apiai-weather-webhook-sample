26ce59f59fff04939fde271399f4dc07165065af
anurag4100 <anuragbharatpandey@gmail.com>
2017-03-29 13:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,17 +38,17 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") == "yahooWeatherForecast":
         
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-elif req.get("result").get("action") == "resolved-rule":
-    print("Got that!!!!!")
+        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+        result = urlopen(yql_url).read()
+        data = json.loads(result)
+        res = makeWebhookResult(data)
+        return res
+    elif req.get("result").get("action") == "resolved-rule":
+        print("Got that!!!!!")
 
 
 
